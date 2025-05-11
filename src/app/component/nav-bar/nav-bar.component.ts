b2936dc3e08084397de772a7fe2696a80b94a234
Idehen Divine <idehendivine16@gmail.com>
2025-05-11 07:08:23 +0100

    feat: implement mobile menu with toggle functionality and backdrop

--- src/app/component/nav-bar/nav-bar.component.ts
+++ src/app/component/nav-bar/nav-bar.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart/cart.component';
 @Component({
   selector: 'app-nav-bar',
-  imports: [CartComponent],
+  imports: [CartComponent, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
 
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+
+    if (this.isMobileMenuOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }
 }
