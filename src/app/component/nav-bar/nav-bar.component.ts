40b01992d7562fafdf88cad9884844fdbc80f1c2
Idehen Divine <idehendivine16@gmail.com>
2025-05-11 07:27:50 +0100

    feat: add viewport check and window resize listener to nav bar component

--- src/app/component/nav-bar/nav-bar.component.ts
+++ src/app/component/nav-bar/nav-bar.component.ts
@@ -1,23 +1,40 @@
-import { Component } from '@angular/core'; 
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart/cart.component';
 @Component({
-  selector: 'app-nav-bar',
-  imports: [CartComponent, CommonModule],
-  templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+	selector: 'app-nav-bar',
+	imports: [CartComponent, CommonModule],
+	templateUrl: './nav-bar.component.html',
+	styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
 
-  isMobileMenuOpen = false;
+	isMobileMenuOpen = false;
 
-  toggleMobileMenu() {
-    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+	toggleMobileMenu() {
+		this.isMobileMenuOpen = !this.isMobileMenuOpen;
 
-    if (this.isMobileMenuOpen) {
-      document.body.classList.add('no-scroll');
-    } else {
-      document.body.classList.remove('no-scroll');
-    }
-  }
+		if (this.isMobileMenuOpen) {
+			document.body.classList.add('no-scroll');
+		} else {
+			document.body.classList.remove('no-scroll');
+		}
+	}
+
+	ngOnInit(): void {
+		this.checkViewport();
+	}
+
+	// Listen to window resize events
+	@HostListener('window:resize', [])
+	onWindowResize() {
+		this.checkViewport();
+	}
+
+	private checkViewport() {
+		if (window.innerWidth > 600) {
+			this.isMobileMenuOpen = false;
+			document.body.classList.remove('no-scroll');
+		}
+	}
 }
